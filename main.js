78b27c834ef00c3804e0a5ca81c585111724ccbc
WangQY1368 <289924569@qq.com>
2022-07-19 20:09:44 +0800

    完成了登录和支付的功能

--- main.js
+++ main.js
@@ -3,27 +3,39 @@ import App from './App'
 import store from '@/store/store.js'
 
 //导入网络请求的包
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
 
 uni.$http = $http
 
 // 配置请求根路径
-$http.baseUrl = 'https://api-ugo-web.itheima.net'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 //请求拦截器
-$http.beforeRequest = function(options){
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
   uni.showLoading({
-    title: '数据加载中...'
+    title: '数据加载中...',
   })
+
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 
 //响应拦截器
-$http.afterRequest = function(){
+$http.afterRequest = function() {
   uni.hideLoading()
 }
 
 //封装的展示消息提示的方法
-uni.$showMsg = function(title = '数据加载失败', duration = 1500){
+uni.$showMsg = function(title = '数据加载失败', duration = 1500) {
   uni.showToast({
     title,
     duration,
@@ -36,7 +48,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-	...App,
+  ...App,
   store
 })
 app.$mount()
